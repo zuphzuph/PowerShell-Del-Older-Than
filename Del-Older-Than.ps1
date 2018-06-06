a067cff5ac4b00df67a3e55b1d664c6523ea5cc7
zuphzuph <zuphzuph@users.noreply.github.com>
2018-06-06 11:31:52 -0600

    Commit Script
    YEET.

---
+++ Del-Older-Than.ps1
@@ -0,0 +1,8 @@
+﻿$Path = "Path to delete from here."
+$DaysBack = "-5" 
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($DaysBack)
+Get-ChildItem -Path $Path -Include *.* -Recurse -Exclude web.config,.htaccess |` 
+Where-Object {$_.LastWriteTime -lt $DatetoDelete} |` 
+Remove-Item -ErrorAction SilentlyContinue -Recurse -Force
+exit
